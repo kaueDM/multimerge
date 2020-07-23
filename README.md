6d8bf722a8237c500532d5ea7aa2c53e6854bccf
kauedm <kaue.maia123@gmail.com>
2020-07-23 14:56:31 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# multimerge action
+
+Action to merge a single branch into multiple branches
+
+## Inputs
+
+### `source`
+
+**Required** Source branch. Default `master`.
+
+### `targets`
+
+**Required** Target branches to merge `source` into.
---
+++ index.js
@@ -0,0 +1,26 @@
+const core = require('@actions/core')
+const github = require('@actions/github')
+
+async function multimerge() {
+  const token = core.getInput('token')
+  const source = core.getInput('source')
+  const targets = core.getInput('targets')
+  const octokit = github.getOctokit(token)
+  const repo = github.context.repo
+
+  try {
+    for (let target of targets) {
+      await octokit.repos.merge({
+        owner: repo.owner,
+        repo: repo.repo,
+        base: target,
+        head: source,
+        commit_message: `[multimerge] Merged '${source}' into '${target}'`
+      })
+    }
+  } catch (e) {
+    core.setFailed(e.message)
+  }
+}
+
+multimerge()
